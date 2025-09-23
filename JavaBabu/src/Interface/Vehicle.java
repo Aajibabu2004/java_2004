a4576d8d199710beb941a7d05a03403f0cd799bf
Aajibabu-dhangar <aajibabud2004@gmail.com>
2025-09-23 11:31:03 +0530

    commit

---
+++ JavaBabu/src/Interface/Vehicle.java
@@ -0,0 +1,24 @@
+package Interface;
+interface Vehicle{
+	void start();
+	void stop();
+}
+class Car implements Vehicle {
+	public void start() {
+		System.out.println("The Car Engine is Started...");
+	}
+	
+	public void stop() {
+		System.out.println("The Car Engine is Stopped...");
+	}
+}
+
+class Bike implements Vehicle {
+	public void start() {
+		System.out.println("The Bike Engine is Started...");
+	}
+	
+	public void stop() {
+		System.out.println("The Bike Engine is Stopped...");
+	}
+}
