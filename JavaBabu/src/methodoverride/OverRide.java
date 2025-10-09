5639b2d61db355ae481d3ce3a5242efdcea150fa
Aajibabu2004 <dhangaraajibabu2004@gmail.com>
2025-10-09 12:23:12 +0530

    commit

---
+++ JavaBabu/src/methodoverride/OverRide.java
@@ -0,0 +1,14 @@
+package methodoverride;
+
+public class OverRide {
+    public void method1(){
+        System.out.println("This is Method 1");
+    }
+}
+
+class Over2 extends OverRide{
+    @Override
+    public void method1(){
+        System.out.println("This is Overriden Method 1");
+    }
+}
---
+++ JavaBabu/src/methodoverride/OverRideMain.java
@@ -0,0 +1,13 @@
+package methodoverride;
+
+public class OverRideMain {
+    public static void main(String[] args) {
+        OverRide or1 = new OverRide();
+        or1.method1();
+
+        Over2 or2 = new Over2();
+        or2.method1();
+
+
+    }
+}
