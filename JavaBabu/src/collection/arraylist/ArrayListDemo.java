49da728b6dc6cfec6a79da0c55d211851506cc7c
Aajibabu2004 <dhangaraajibabu2004@gmail.com>
2025-10-09 10:23:47 +0530

    commit

---
+++ JavaBabu/src/collection/arraylist/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<Integer> ar = new ArrayList<>();
+		
+		ar.add(3);
+		ar.add(4);
+		ar.add(5);
+		ar.remove(2);
+		
+		
+		System.out.print(ar);
+
+	}
+
+}
---
+++ JavaBabu/src/collection/arraylist/ArrayListMain.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.ArrayList;
+
+public class ArrayListMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	ArrayList al = new ArrayList();
+		
+		al.add(3);
+		al.add("Aaji babu dhangar");
+		al.add(null);
+		al.add(true);
+		
+		System.out.print(al);
+
+
+	}
+
+}
