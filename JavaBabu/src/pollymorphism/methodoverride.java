5696b08480e3990aab0189301cd3037cd4ba6b42
Aajibabu2004 <dhangaraajibabu2004@gmail.com>
2025-10-09 11:54:11 +0530

    commit

---
+++ JavaBabu/src/pollymorphism/methodoverride.java
@@ -0,0 +1,23 @@
+package polymorphism;
+
+class Override{
+	public void show()
+	{
+		System.out.println("this is parent class");
+	}
+}
+class child extends Override{
+	public void show()
+	{
+	System.out.println("this is child class");
+	}
+}
+public class methodoverride {
+
+	public static void main(String[] args) {
+	 child c=new child();
+	 c.show();
+
+	}
+
+}
