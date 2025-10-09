6f84c02dd62357b1884d587084a53c140d091f6e
Aajibabu2004 <dhangaraajibabu2004@gmail.com>
2025-10-09 12:21:49 +0530

    commit

---
+++ JavaBabu/src/methodoverload/MethodOverloadMain.java
@@ -0,0 +1,11 @@
+package methodoverload;
+
+public class MethodOverloadMain {
+    public static void main(String[] args) {
+        MethodOverload ml = new MethodOverload();
+
+        ml.show();
+        System.out.println(ml.show(10));
+        ml.show(1,2);
+    }
+}
